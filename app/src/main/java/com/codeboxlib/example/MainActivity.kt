818e22ba480b93bd34b0486826d7a705dbf41149
vipafattal <vipafattal@gmail.com>
2019-04-08 10:11:08 +0300

    Fixing Lib crash

--- app/src/main/java/com/codeboxlib/example/MainActivity.kt
+++ app/src/main/java/com/codeboxlib/example/MainActivity.kt
@@ -2,13 +2,13 @@ package com.codeboxlib.example
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.codebox.lib.android.os.AppContextProvider
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-
+        AppContextProvider.init(this)
     }
 }
